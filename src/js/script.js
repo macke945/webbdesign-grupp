a7892d0bb67734143cf436afc5ab26d9eb497423
EricvandenOutenaar <55479562+EricvandenOutenaar@users.noreply.github.com>
2020-11-03 21:22:50 +0100

    completed the payment flow

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,15 @@ var EventHandlers = (function () {
     function init() {
         $(".checkout-modal").hide();
         $(".checkout__details--button").click(onClickCheckoutConfirm);
-         
-          
        
     }
 
     function onClickCheckoutConfirm(e) { 
         $(".checkout__details").hide();
         $(".checkout-modal").show();
-          
+        
+            alert('Tack för din beställing. Här låtsas vi att det pågår en redirekt till valde betaltjänst. Klicka på ok för att låtsas att allt har gått korrekt');
+           
     }
      
    
