1700806fee5f7fae9567e4c383abe661364a2f62
Khaled El Hamzi <khaled.elhamzi.syne19lin@tucsweden.se>
2020-11-03 23:06:31 +0100

    Updated checkout form & styled thanks page

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,16 @@
 // In this module I have placed the events. 
 var EventHandlers = (function () {
 
-   
     function init() {
         // we can take away this
-        $(".checkout-modal").hide();
-    
+        $(".checkout-modal").hide();   
         $("form").submit(function(e) {
             alert('Tack för din beställing. Här låtsas vi att det pågår en redirekt till valde betaltjänst. Klicka på ok för att låtsas att allt har gått korrekt');
             window.location.href ="./thanks.html"
             return false;
         });
-        
-       
     }
- 
-     
-   
+
     return {
         init
     }
@@ -24,5 +18,4 @@ var EventHandlers = (function () {
 
 $(document).ready(function () {
     EventHandlers.init();
-  
 });
